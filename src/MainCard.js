ff6edb0e05f615b296f6b7d20b7b51358a63b399
DeppyPanos <despinna2009@gmail.com>
2022-10-26 18:51:25 +0100

    add onclick to main card

--- src/MainCard.js
+++ src/MainCard.js
@@ -2,16 +2,23 @@ import Card from 'react-bootstrap/Card';
 import './MainCard.css';
 
 function MainCard() {
+
+  function handleClick() {
+    alert("I got clicked!")
+  }
+
   return (
-    <Card className="menu" style={{ width: '15rem' }}>
+    <Card className="menu" onClick={() => handleClick()} style={{ width: '15rem' }}>
       <Card.Body>
         <Card.Text className="card-text">
             FOOD & DRINKS
         </Card.Text>
-        <Card.Img className="card-img" variant="top" src="https://img.icons8.com/color/96/000000/tomato.png" />
+        <Card.Img className="card-img" variant="top" src="https://img.icons8.com/color/96/000000/meal--v1.png" />
       </Card.Body>
     </Card>
   );
 }
 
 export default MainCard;
+
+
