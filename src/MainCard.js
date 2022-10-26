eb2a532e1e8656dcac06bc79bcfcec411ee1d384
Abigail Unwin <abigailunwin@gmail.com>
2022-10-26 18:11:17 +0100

    card update

--- src/MainCard.js
+++ src/MainCard.js
@@ -2,15 +2,29 @@ import Card from 'react-bootstrap/Card';
 import './MainCard.css';
 
 function MainCard() {
+
+  const cards = [
+    {title: 'FOOD & DRINKS', img: "https://img.icons8.com/color/96/000000/tomato.png"},
+    {title: 'ACTIVITIES', img: "test"},
+    {title: 'NEEDS', img: "test"},
+    {title: 'EMOTIONS', img: "test"}
+  ]
+
   return (
-    <Card className="menu" style={{ width: '15rem' }}>
-      <Card.Body>
-        <Card.Text className="card-text">
-            FOOD & DRINKS
-        </Card.Text>
-        <Card.Img className="card-img" variant="top" src="https://img.icons8.com/color/96/000000/tomato.png" />
-      </Card.Body>
-    </Card>
+    <div className="card-container">
+      {cards.map((card) => (
+      <Card className="menu" style={{ width: '15rem' }}>
+        <Card.Body>
+          {
+            <Card.Text className="card-text">
+              <p key={card.title}>{card.title}</p>
+            </Card.Text>
+          }
+          <Card.Img className="card-img" variant="top" src={card.img} />
+        </Card.Body>
+      </Card>
+      ))};
+    </div>
   );
 }
 
