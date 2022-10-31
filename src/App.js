5f095c51c3c8be5c90f9947fd9998c7e7cbcd92f
DeppyPanos <despinna2009@gmail.com>
2022-10-31 12:12:29 +0000

    add changes -check maincard and app.js-

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './Footer';
 import Header from './Header';
 import MainCard from './MainCard';
+import {Container, Row, Col} from 'react-bootstrap';
+
 
 function App() {
   return (
@@ -12,8 +14,25 @@ function App() {
       <Header/>
 
       <main className="App-main" style={{ backgroundImage: `url(${background})` }}>
-      <MainCard className="Main-card"/>
 
+      <Container>
+        <Row>
+          <Col md><MainCard className="Main-card" title="FOOD & DRINKS" img="https://img.icons8.com/color/96/000000/meal.png"/></Col>
+          <Col md><MainCard className="Main-card" title="ACTIVITIES" img="https://img.icons8.com/color/96/000000/jump-skin-type-2.png"/></Col>
+        </Row>
+        <Row >
+          <Col md><MainCard className="Main-card" title="NEEDS" img="https://img.icons8.com/color/96/000000/toilet-bowl.png"/></Col>
+          <Col md><MainCard className="Main-card" title="EMOTIONS" img="https://img.icons8.com/color/96/000000/smiling-face-with-heart.png"/></Col>
+        </Row>
+      </Container>
+     
+
+
+
+      
+      
+      
+      
       </main>
       
       <Footer/>
--- src/Footer.js
+++ src/Footer.js
@@ -5,6 +5,7 @@ function Footer() {
   return (
     <footer>
       <p>Copyright ⓒ {year}</p>
+      <p className="attribute">Icons by <a href="https://icons8.com/" target={"_blank"}>Icons8</a></p>
     </footer>
   );
 }
